9ad5c6c9ebf896f72edf3a81867dc2a2a1ff3cb0
Edem Ekeng <edemekeng@Edems-Mac-mini.local>
2021-12-06 00:04:38 -0500

    Search Landing in progress, primary layout completed

---
+++ YellowPages/Components/Design/AppAssets.swift
@@ -0,0 +1,12 @@
+//
+//  AppAssets.swift
+//  YellowPages
+//
+//  Created by Edem Ekeng on 2021-12-05.
+//
+
+import Foundation
+
+enum CustomImages : String {
+    case searchBaseLanding = "searchBaseLanding"
+}
